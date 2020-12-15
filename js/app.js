074147f1f38c5bf19e82c3b5cbb22c527715275b
Giana Mylan <Giana Mylan>
2020-12-15 18:19:52 -0500

    almost done

--- js/app.js
+++ js/app.js
@@ -1,27 +1,3 @@
-// make array for the board 
-//player1 = 'X' 
-//player2 = "computer or 'O'"
-
-
-
-let boardArray = ["","","","","","","","",""];
-var player1Turn = true;
-
-/*
-function startGame() {
-    let p1Name = document.getElementById("inputPlayer1").value;
-    document.getElementById("player1Score").innerHTML = p1Name;
-    let p2Name = document.getElementById("inputPlayer2").value;
-    document.getElementById("player2Score").innerHTML = p2Name;
-    
-    if(player1 === "X") {
-        $(this).html("X")
-    }
-    if(player2 === "O") {
-        $(this).html("O")
-    }
-};*/
-
 const classNames = {
     cell: 'cell',
     cellValue: 'cell-value',
@@ -30,97 +6,104 @@ const classNames = {
 
 const users = {
     playerOne: 'X',
-    playerTwo: 'O'
+    playerTwo: 'O',
 }
 
+const winType = {
+    tie: 'tie'
+}
+
+let boardArray = ["","","","","","","","",""];
+var player1Turn = true;
+let winner;
+let turnsRemain = 9;
+
+const winArrays = {
+    0: [[1,2], [3,6],[4,8]],
+    1: [[0,2],[4,7]],
+    2: [[0,1],[5,6], [4,6]],
+    3: [[0,6],[4,5]],
+    4: [[0,8],[6,2],[1,7],[3,5]],
+    5: [[2,8],[3,4]],
+    6: [[0,3],[7,8],[4,2]],
+    7: [[6,8],[1,4]],
+    8: [[6,7],[0,4],[2,5]]
+};
+
 const cell = document.querySelectorAll(`.${classNames.cell}`);
+const winnerOverlay = document.querySelector(`#winner-overlay`);
+const whoWon = document.querySelector(`#winnerBox > span`) 
+const resetButton = document.querySelector(`#resetButton`)
+
+const findWinner = (index) => {
+    const winRange = winArrays[index];
+
+    for(let i = 0; i < winRange.length; i++) {
+        const currentChoice = boardArray[index];
+        const choiceOne = boardArray[winRange[i][0]];
+        const choiceTwo = boardArray[winRange[i][1]];
+
+        if(currentChoice === choiceOne && choiceOne === choiceTwo) {
+            winner = currentChoice;
+            return true;
+        }
+    }
+    if (turnsRemain === 0) {
+        winner = winType.tie;
+        return true;
+    }
+
+    return false;
+};
+
+const showWinner = () => {
+    if (winner === winType.tie) {
+        whoWon.innerHTML = `It was a tie!`
+    } else {
+        whoWon.innerHTML = `"Winner is ${winner}!"`;
+    } 
+    winnerOverlay.style.display = 'grid';
+};
+
+function resetBoard () {
+    boardArray = ["","","","","","","","",""];
+    player1Turn = true;
+    winner = undefined;
+    turnsRemain = 9;
+
+    cell.forEach ((e, i) => {
+        const cellValue = e.querySelector(`.${classNames.cellValue}`);
+        cellValue.innerHTML = boardArray[i];
+        cellValue.classLists.remove(classList.animation);
+        e.classList.remove(classNames.winner);
+    });
+
+    winnerOverlay.style.display = 'none';
+}
+
+resetButton.addEventListener('click', () => {
+    resetBoard();
+});
+
 cell.forEach((e, i) => {
     e.addEventListener('click', () => {
         if (!boardArray[i]) {
         boardArray[i] = player1Turn ? users.playerOne : users.playerTwo;
         player1Turn = !player1Turn;
+        turnsRemain--;
+
+        if (findWinner(i)) {
+            showWinner();
+        }
 
         const cellValue = e.querySelector(`.${classNames.cellValue}`);
         cellValue.innerHTML = boardArray[i];
-        cellValue.classLists.add(classNames.animation);
+        cellValue.classLists.add(classNames.cell-value.animation); //ask about this add 
+        //why is the add undefined
         }
     });
 });
 
-/*
-function cellClick(hi) {
-   document.getElementById(hi)
-   //console.log(document.getElementById(hi).innerHTML)
-   if(turn) {
-    document.getElementById(hi).innerHTML = "X"
-   } else {
-    document.getElementById(hi).innerHTML = "O"
-   } turn =! turn
-   console.log(hi) 
-}; */
-
-function trackGame () {
-}
-
-function countPlayer1 () { 
-    let p1Count = $('.board, "X"').length  
-    return p1Count
-};
-
-function countPlayer2 () {
-    let p2Count = $('.board, "O"').length
-    return p2Count
-};
-
-function gameTrack () {
-
-} // check 
-
-/*
-function whoWonGame () {
-    const winArr = [
-    ["#1, #2, #3"], ["#4, #5, #6"], ["#7, #8, #9"], ["#1, #4, #7"], ["#2, #5, #8"], ["#3, #6, #9"], ["#1, #5, #9"], ["#3, #5, #7"]];
-
-    let player1WinArr = getWinArr(winArr, "X");
-    let player2WinArr = getWinArr(winArr, "O");
-    let p1Wins = player1WinArr.includes(true);
-    let p2Wins = player2WinArr.includes(true);
-    let fullBoard = countPlayer1() + countPlayer2()
-    let drawGame = (fullBoard === 9) && (!player1Wins) && (!player2Wins) 
 
 
-
-    if(p1Wins) {
-        player1WinArr()
-        return winner = `${Player1Input} wins!}`;
-    }
-    if(p2Wins) {
-        player2WinArr()
-        return winner = `${Player2Input} wins!}`
-    }
-    if(drawGame) {
-        drawGame() 
-        return winner = draw
-    } else {
-        console.log ('the show must go on...')
-    }
-};
-*/
-
-
-function resetBoard () {
-    console.log(document.querySelectorAll('.cell'))
-    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "Choose Me!");
-}
-
-
-
-// click function for the cells 
-//document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClick));
-
-//click function for the restart button
-//document.querySelector('.restartButton').forEach(cell => cell.innerHTML = "Choose!");
-
-//click function for the start button
-//document.querySelector('.goButton').addEventListener('click', startGame);
 
